2a4c62a146d77c88ea36740e69a4d9ae48d4bdb5
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-06-23 17:57:18 +0400

    Revised the solution to do one less check

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -9,9 +9,7 @@ var Year = function(year) {
 
 Year.prototype.isLeap = function() {
 	if (this.year % 4 === 0) {
-		if (this.year % 100 && this.year % 400 === 0) {
-			return true;
-		} else if (this.year % 100 && this.year % 400 !== 0) {
+		if (this.year % 100 && this.year % 400 !== 0) {
 			return false;
 		} else {
             return true;
